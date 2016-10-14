dd4074df4b4dcac9dd831ad72cd30bdfc3659719
mmatt <m.matt.barry@gmail.com>
2016-10-14 19:13:55 -0400

    set whitelist for file uploads to only be PDFs

--- app/resources/song_resource.rb
+++ app/resources/song_resource.rb
@@ -7,6 +7,9 @@ class SongResource < JSONAPI::Resource
   has_many :languages, acts_as_set: true
   has_many :tags, acts_as_set: true
 
-  filters :parts_no, :has_text
+  filters :parts_no
 
+  def self.sortable_fields(context)
+    super(context) << :"book.name"
+  end
 end
