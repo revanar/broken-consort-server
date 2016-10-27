7ada7efa7dea3d7aaf82c457efc506cf982a6c26
mmatt <m.matt.barry@gmail.com>
2016-10-27 10:18:35 -0400

    added thumbnail generation for uploaded PDFs.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'puma', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+
+
 gem 'jsonapi-resources'
 
 # Use Capistrano for deployment
@@ -33,7 +35,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'carrierwave'
+gem 'rmagick'
+gem 'carrierwave', require: ['carrierwave/processing/rmagick', 'carrierwave/processing/mime_types']
 gem 'carrierwave-sequel', :require => 'carrierwave/sequel'
 gem 'carrierwave-base64'
 gem 'mini_magick'
--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -3,7 +3,7 @@
 class PdfUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick, CarrierWave::MimeTypes
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -40,6 +40,27 @@ class PdfUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
   # end
+
+  def cover
+    manipulate! do |frame, index|
+      frame if index.zero?
+    end
+  end
+
+  version :thumb do
+    process :cover
+    process :convert => 'png'
+    process :resize_to_fit => [200, 300]
+    process :set_content_type_png
+    def full_filename (for_file = model.document.file)
+      super.chomp(File.extname(super)) + '.png'
+    end
+  end
+
+  def set_content_type_png(*args)
+    Rails.logger.debug "#{file.content_type}"
+    self.file.instance_variable_set(:@content_type, "image/png")
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
